4e8ef5268302495b520fa1471901554ffd6f803e
Taeyang Jin (Heli) <sun@dataportal.kr>
2022-11-27 01:00:53 +0900

    refactor: add requiredId on BaseDomain

--- consortium-calendar-assets/consortium-calendar-assets-domain/src/main/kotlin/kr/dataportal/calendar/config/Domain.kt
+++ consortium-calendar-assets/consortium-calendar-assets-domain/src/main/kotlin/kr/dataportal/calendar/config/Domain.kt
@@ -1,5 +1,6 @@
 package kr.dataportal.calendar.config
 
+import kr.dataportal.calendar.notNull
 import kr.dataportal.calendar.persistence.config.jpa.BaseEntity
 import kr.dataportal.calendar.persistence.config.jpa.requiredId
 import java.time.LocalDateTime
@@ -13,6 +14,9 @@ abstract class BaseDomain {
     lateinit var createdAt: LocalDateTime
     lateinit var lastModifiedAt: LocalDateTime
 }
+
+val BaseDomain.requiredId: Long
+    get() = id.notNull { "Domain(${javaClass.simpleName}) id is null" }
 
 inline fun <reified T : BaseDomain> T.entityData(entity: BaseEntity): T {
     id = entity.requiredId
--- consortium-calendar-assets/consortium-calendar-assets-interface/src/main/kotlin/kr/dataportal/calendar/api/account/dto.kt
+++ consortium-calendar-assets/consortium-calendar-assets-interface/src/main/kotlin/kr/dataportal/calendar/api/account/dto.kt
@@ -1,6 +1,7 @@
 package kr.dataportal.calendar.api.account
 
 import kr.dataportal.calendar.account.domain.Account
+import kr.dataportal.calendar.config.requiredId
 import javax.validation.constraints.NotBlank
 
 /**
@@ -26,6 +27,7 @@ data class LoginPlainAuthAccountDto(
 )
 
 data class AccountResponseDto(
+    val id: Long,
     val name: String,
     val email: String,
     val phoneNumber: String
@@ -34,6 +36,7 @@ data class AccountResponseDto(
 
 // ====================================
 internal fun Account.toResponseDto() = AccountResponseDto(
+    id = requiredId,
     name = name,
     email = email,
     phoneNumber = phoneNumber
